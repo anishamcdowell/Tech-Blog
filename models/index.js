e8d263bf5855bddd706cca6390e95b349059061b
Anisha McDowell <anishamcdowell@gmail.com>
2021-05-01 21:36:09 -0400

    updated port

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const User = require('./users');
+const Post = require('./posts');
+const Comment = require('./comments');
+
+//associations
+// Post.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete: "CASCADE"
+// });
+
+
+module.exports = { User, Post, Comment };
